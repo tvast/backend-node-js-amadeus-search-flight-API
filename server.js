db2441f0270a3e0316a1b011e0d37cd1b048a036
theo <theophile.vast@amadeus.com>
2020-01-08 10:44:44 +0100

    initialize search price

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 
 const fetch = require('node-fetch');
 
- app = express(),
- port = process.env.PORT || 3000;
+app = express(),
+port = process.env.PORT || 3000;
 
 
 let allowCrossDomain = function(req, res, next) {
@@ -14,127 +14,106 @@ let allowCrossDomain = function(req, res, next) {
   next();
 }
 app.use(allowCrossDomain);
-let Amadeus = require('amadeus');
-
-let amadeus = new Amadeus({
-  clientId: 'qztkbf5XWjNSGkXRF9bfAwNg6bELWvVD',
-  clientSecret: 'w9mJ7ZJzlEGNffut'
-});
 
 const uriAuth ="https://test.api.amadeus.com/v1/security/oauth2/token" 
-
+//initialize variable
 let departure ="2020-02-01";
 let arrival="2020-03-30";
-let locationDeparture="MAD"
-let locationArrival="PAR"
+let locationDeparture="MAD";
+let locationArrival="PAR";
+let flightfrommadrid="";
+let flightfrommadrid2="";
+let flightfrommadrid3="";
 //get grant
-let headers= {
-      // 'Content-Type': 'application/json'   
+let headers= {  
       'Content-Type': 'application/x-www-form-urlencoded',
-     };
+    };
 
-let body = {
-   "grant_type": "client_credentials",
-   "client_id": "qztkbf5XWjNSGkXRF9bfAwNg6bELWvVD",
-  "client_secret": "w9mJ7ZJzlEGNffut",
-  
-}
+    let body = {
+     "grant_type": "client_credentials",
+     "client_id": "qztkbf5XWjNSGkXRF9bfAwNg6bELWvVD",
+     "client_secret": "w9mJ7ZJzlEGNffut",
 
-let token="";
-let flightfrommadrid ="";
-fetch(uriAuth, { method: 'POST', 
-  headers: headers, 
-  body: 'grant_type=client_credentials&client_id=' + body.client_id + '&client_secret=' + body.client_secret
-})
-  .then((res) => {
+   }
+
+   let token="";
+   fetch(uriAuth, { method: 'POST', 
+    headers: headers, 
+    body: 'grant_type=client_credentials&client_id=' + body.client_id + '&client_secret=' + body.client_secret
+  })
+   .then((res) => {
      return res.json()
-})
-.then((json) => {
-  console.log(json);
-  token=json.access_token;
-  console.log(token);
+   })
+   .then((json) => {
+    console.log(json);
+    token=json.access_token;
+    console.log(token);
 
 
 //POST flight search 
 
 });
-let header= {'content-type': 'application/json', authorization: 'Bearer '+token}
 
+let header= {'content-type': 'application/json', authorization: 'Bearer '+token}
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//get flight offer
 app.post('/date', function(req, res) {
-  
-    departure = req.body.departure;
-    arrival = req.body.arrival;
-    locationDeparture = req.body.locationDeparture;
-    locationArrival =req.body.locationArrival;
-    
-    updateFlightSearch(departure, arrival, locationArrival,locationDeparture);
-    console.log(departure, arrival, locationArrival,locationDeparture);
-    // console.log(departure+" "+arrival)
-    // res.send(departure + ' ' + arrival);
+  departure = req.body.departure;
+  arrival = req.body.arrival;
+  locationDeparture = req.body.locationDeparture;
+  locationArrival =req.body.locationArrival;
+  updateFlightSearch(departure, arrival, locationArrival,locationDeparture);
+  }); 
+//get flight offer price
 
-}); 
-
-
-
-// let uriFlightOffer = "https://test.api.amadeus.com/v2/shopping/flight-offers"
-// // console.log(request);
-// fetch(uriFlightOffer, { method: 'POST', 
-//   headers: header, 
-//   body: JSON.stringify(request)
-// })
-//   .then((res) => {
-//      return res.json()
-// })
-// .then((json) => {
-//   // console.log(json);
-//   flightfrommadrid=json.data;
-//   // Do something with the returned data.
-// });
+app.post('/flightprice', function(req, res) {
+  inputFlight = req.body.inputFlight;
+  flifghtPrice(inputFlight);
+  }); 
+//flight search
 
 let updateFlightSearch = function (departure, arrival, locationArrival,locationDeparture) {
-// flightfrommadrid2="";
 let header= {'content-type': 'application/json', authorization: 'Bearer '+token}
-
 let request = {
   "currencyCode": "USD",
   "originDestinations": [
-    {
-      "id": "1",
-      "originLocationCode": locationDeparture,
-      "destinationLocationCode": locationArrival,
-      "departureDateTimeRange": {
-        "date": departure,
-        "time": "10:00:00"
-      }
-    },
-   ],
+  {
+    "id": "1",
+    "originLocationCode": locationDeparture,
+    "destinationLocationCode": locationArrival,
+    "departureDateTimeRange": {
+      "date": departure,
+      "time": "10:00:00"
+    }
+  },
+  ],
   "travelers": [
-    {
-      "id": "1",
-      "travelerType": "ADULT",
-      "fareOptions": [
-        "STANDARD"
-      ]
-    },
+  {
+    "id": "1",
+    "travelerType": "ADULT",
+    "fareOptions": [
+    "STANDARD"
+    ]
+  },
   ],
   "sources": [
-    "GDS"
+  "GDS"
   ],
   "searchCriteria": {
     "maxFlightOffers": 50,
     "flightFilters": {
       "cabinRestrictions": [
-        {
-          "cabin": "BUSINESS",
-          "coverage": "MOST_SEGMENTS",
-          "originDestinationIds": [
-            "1"
-          ]
-        }
+      {
+        "cabin": "BUSINESS",
+        "coverage": "MOST_SEGMENTS",
+        "originDestinationIds": [
+        "1"
+        ]
+      }
       ],
-     }
+    }
   }
 }
 
@@ -144,13 +123,34 @@ fetch(uriFlightOffer, { method: 'POST',
   headers: header, 
   body: JSON.stringify(request)
 })
-  .then((res) => {
-     return res.json()
+.then((res) => {
+ return res.json()
 })
 .then((json) => {
   console.log(json);
   flightfrommadrid2=json.data;
   return flightfrommadrid2;
+  // Do something with the returned data.
+});  
+}
+
+let flifghtPrice = function (inputFlightOffer) {
+let header= {'content-type': 'application/json', authorization: 'Bearer '+token}
+
+
+let uriFlightOffer = "https://test.api.amadeus.com/v2/shopping/flight-offers/pricing"
+// console.log(request);
+fetch(uriFlightOffer, { method: 'POST', 
+  headers: header, 
+  body: JSON.stringify(inputFlightOffer)
+})
+.then((res) => {
+ return res.json()
+})
+.then((json) => {
+  console.log(json);
+  flightfrommadrid3=json.data;
+  return flightfrommadrid3;
   // Do something with the returned data.
 });  
 }
@@ -164,6 +164,11 @@ app.get('/flight', function(req, res) {
 app.get('/flightSearch', function(req, res) {
   res.send(JSON.stringify(flightfrommadrid2));
   console.log(flightfrommadrid2)
+});
+
+app.get('/flightPrice', function(req, res) {
+  res.send(JSON.stringify(flightfrommadrid3));
+  console.log(flightfrommadrid3)
 });
 
 app.get('/token', function(req, res) {
